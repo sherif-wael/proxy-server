9b1e4d5461e3829269bcf7c5736cc0020bd0d469
sherif-wael <sherifwael80@gmail.com>
2022-03-01 23:24:29 +0200

    Add cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.use("/cors", createProxyMiddleware({
     changeOrigin: true,
