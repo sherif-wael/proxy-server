467077a6f08b19bed2ccbe061560fe4eeccadad0
Sherif Wael <sherifwael80@gmail.com>
2022-03-11 19:26:15 +0200

    a fixed router

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(cors());
 app.use("/cors", createProxyMiddleware({
     changeOrigin: true,
     target: "https://beta.idataworkers.com",
-    router: req => {
-        const { host, protocol } = url.parse(req.query.url, true);
-        return `${protocol}//${host}`;
-    },
+    // router: req => {
+    //     const { host, protocol } = url.parse(req.query.url, true);
+    //     return `${protocol}//${host}`;
+    // },
     pathRewrite: (_, req) => {
         const { url: proxyURL, ...rest } = req.query;
         const { pathname,  query } = url.parse(proxyURL);
